b4b4c5816ab2f3fe1facfb412efbc8fff85ac078
keenanh17 <hubbark8@my.erau.edu>
2024-05-30 10:49:11 -0400

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+ball_height = 100 # Meters
+num_bounces = 0
+# next_height = (3/5) * ball_height
+
+while num_bounces < 10 :
+    num_bounces = num_bounces + 1  
+    ball_height = (3/5) * ball_height
+    print(num_bounces , round(ball_height , 4))
